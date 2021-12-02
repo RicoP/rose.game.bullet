dd74293fd82a403a51c509272fa9fd2794f132f5
Rico P <RicoP@users.noreply.github.com>
2021-12-02 15:15:42 +0100

    bullet

--- externals/clone.bat
+++ externals/clone.bat
@@ -1,4 +1,5 @@
 git clean -xdff .
+git clone --depth 1 https://github.com/bulletphysics/bullet3.git
 git clone --depth 1 https://github.com/raysan5/raylib.git
 git clone --depth 1 https://github.com/RicoP/mathc.git include/mathc
 git clone --depth 1 https://github.com/RicoP/premake-comppp.git
