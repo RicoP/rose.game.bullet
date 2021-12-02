705353e1fccda0cc38b9da7c67699f424272afa2
Rico P <RicoP@users.noreply.github.com>
2021-12-02 15:14:21 +0100

    externals

---
+++ externals/clone.bat
@@ -0,0 +1,5 @@
+git clean -xdff .
+git clone --depth 1 https://github.com/raysan5/raylib.git
+git clone --depth 1 https://github.com/RicoP/mathc.git include/mathc
+git clone --depth 1 https://github.com/RicoP/premake-comppp.git
+git clone --depth 1 https://github.com/RicoP/roselib.git
